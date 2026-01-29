cd9a6a423dd49b88851d01d597f68e72b7fca875
Dan Nomen <dan.nomen@gmail.com>
2026-01-29 10:57:24 +0300

    Mise a jour Frontend (couleur verte sidebar)

--- frontend/src/api_config.js
+++ frontend/src/api_config.js
@@ -1,5 +1,5 @@
 // Détection automatique : si localhost on utilise le port 8000, sinon on utilise le serveur actuel
-const API_BASE_URL = window.location.hostname === "localhost"
+const API_BASE_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
     ? "http://localhost:8000"
     : "";
 
